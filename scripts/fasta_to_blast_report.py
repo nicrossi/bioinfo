cec1eb469a137a98d6b3302e0c2bfcb6cba2b1d9
Nico Rossi <nrossi@itba.edu.ar>
2023-11-16 10:39:54 -0300

    fix: fasta_to_blast_report - choosing longest ORF before doing blast

--- scripts/fasta_to_blast_report.py
+++ scripts/fasta_to_blast_report.py
@@ -1,7 +1,10 @@
+import os
 import sys
 import subprocess
 import threading
 import time
+import re
+
 
 # Run child process to execute the BLAST search
 def run_command(cmd):
@@ -18,7 +21,8 @@ def print_dots(exit_signal):
 
 # Executes the BLAST search in the provided database and print result on a .out file
 def to_blast(input_file, db_name, out_name):
-    cmd = f"blastp -query {input_file} -db {db_name} -out {out_name}.out -outfmt '6 std qlen slen'"
+    longest = get_longest_orf(input_file)
+    cmd = f"blastp -query {longest} -db {db_name} -out {out_name}.out -outfmt '6 std qlen slen'"
 
     print("Fasta to Blast report:")
     print(f"Processing input {input_file}")
@@ -32,10 +36,68 @@ def to_blast(input_file, db_name, out_name):
     if result.returncode == 0:
         exit_signal.set()
         dots_thread.join()
+        clean_tmp_file(longest)
         return 0
     else:
         return -1
 
+
+def extract_len_from_header(header):
+    match = re.search(r'len (\d+)', header)
+    if match:
+        return int(match.group(1))
+    else:
+        return None
+
+
+# Clean up temporary file
+def clean_tmp_file(tmp_filename):
+    try:
+        os.remove(tmp_filename)
+    except Exception as e:
+        print(f"Error while removing temporary file: {e}")
+
+
+def get_longest_orf(fastafile):
+    """
+    Reads a fasta file and save the longest ORF
+    translation in a temp fasta file to use on the BLAST report
+    """
+    sequences = []
+    with open(fastafile, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            sequences.append(line.rstrip("\n"))
+
+    max_len = 0
+    max_seq_id = None
+    seq_id = []
+    seq_dic = {}
+
+    for i in range(len(sequences)):
+        if sequences[i][0] == ">":
+            seq_id.append(sequences[i])
+            curr_len = extract_len_from_header(sequences[i])
+            if curr_len > max_len:
+                max_len = curr_len
+                max_seq_id = sequences[i]
+
+    if max_seq_id:
+        seq_id_index = [sequences.index(seq_id[i]) for i in range(len(seq_id))]
+        for i in range(len(seq_id_index)):
+            if i == (len(seq_id_index) - 1):
+                seq_dic[seq_id[i]] = "".join(sequences[seq_id_index[i] + 1:])
+            else:
+                seq_dic[seq_id[i]] = "".join(sequences[seq_id_index[i] + 1:seq_id_index[i + 1]])
+
+        # Create a temporary FASTA file with the longest sequence
+        temp_fasta_file = "temp_longest_orf.fasta"
+        with open(temp_fasta_file, "w") as temp_fasta:
+            temp_fasta.write(f">{max_seq_id}\n{seq_dic[max_seq_id]}")
+
+        return temp_fasta_file
+    else:
+        raise Exception(f"Error while reading input file {fastafile}")
 
 
 def main():
